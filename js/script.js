01d1a8b4f759d21202e3e8d65766c5075e7a3090
lam <i.mailed.lam@gmail.com>
2023-12-25 20:16:47 +0200

    OBjects & methods ex2

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const bird = {
+  name: "Eliza",
+  type: "Budgie",
+  hungerScale: 5, // Scale from 0 (full) to 10 (starving)
+  isHappy: function () {
+    if (this.hungerScale <= 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+bird.eat = function() {
+  if (this.hungerScale === 0) {
+    console.log(`${this.name} is full and cant eat more`);
+  } else {
+    this.hungerScale -= 1;
+    console.log("Yummmmmmmy yum !");
+  }
+};
+
+bird.eat()
+//console.log(bird.isHappy());
+
+bird.eat()
+//console.log(bird.isHappy());
+
+bird.eat()
+//console.log(bird.isHappy);
+
+bird.eat()
+//console.log(bird.isHappy);
+
+bird.eat()
+console.log(bird.isHappy);
+
+
+
+
+
+
